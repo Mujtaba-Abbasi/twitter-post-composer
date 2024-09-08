bbdbdc4b09b75204f4e15085c36f45e0f1ab6f8a
= <=>
2024-09-08 23:35:03 +0500

    Hide post composer navlink when un-authenticated

--- app/(routes)/(home)/components/TwitterConnect/index.tsx
+++ app/(routes)/(home)/components/TwitterConnect/index.tsx
@@ -1,25 +1,39 @@
-'use client'
+"use client";
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
-import {signIn, signOut, useSession} from 'next-auth/react' ;
-import {Card, CardContent} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Card, CardContent } from "@/components/ui/card";
 
 export const TwitterConnect = () => {
-    const session = useSession();
-    const isAuthenticated = session.status === 'authenticated';
+  const session = useSession();
+  const isAuthenticated = session.status === "authenticated";
 
-    return (
-        <Card className="self-center lg:self-start min-w-[300px] h-[250px] radial-gradient  rounded-xl border-[0.5px] border-gray-600">
-            <CardContent className="flex flex-col h-full justify-between items-center">
-                    <div className='flex flex-col gap-2 h-full justify-center'>
-                        <Image src={session?.data?.user?.image ? session.data.user.image : "/svgs/twitter.svg"} height={55} width={55} alt="twitter" className='rounded-full' />
-                        <span className="text-white font-medium">Twitter</span>
-                    </div>
+  return (
+    <Card className="self-center lg:self-start min-w-[300px] h-[250px] radial-gradient  rounded-xl border-[0.5px] border-gray-600">
+      <CardContent className="flex flex-col h-full justify-between items-center">
+        <div className="flex flex-col gap-2 h-full items-center justify-center">
+          <Image
+            src={
+              session?.data?.user?.image
+                ? session.data.user.image
+                : "/svgs/twitter.svg"
+            }
+            height={55}
+            width={55}
+            alt="twitter"
+            className="rounded-full"
+          />
+          <span className="text-white text-lg">Twitter</span>
+        </div>
 
-                <Button onClick={() => isAuthenticated ? signOut() : signIn()} size='lg' className="w-full text-md radial-gradient-secondary border-[0.5px] border-gray-600 text-white py-5 rounded-xl">
-                    {isAuthenticated ? 'Signout' : 'Connect'}
-                </Button>
-            </CardContent>
-        </Card>
-    )
-}
+        <Button
+          onClick={() => (isAuthenticated ? signOut() : signIn())}
+          size="lg"
+          className="w-full text-md radial-gradient-secondary border-[0.5px] border-gray-600 text-white py-5 rounded-xl"
+        >
+          {isAuthenticated ? "Signout" : "Connect"}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
