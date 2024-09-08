0c261604402d0f4cb29a812152e38822415bfe88
= <=>
2024-09-08 23:37:00 +0500

    Add token refresh, update the permission scope and add callbacks for exposing access token to session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,86 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
-
 const handler = NextAuth({
-    providers: [
-        TwitterProvider({
-            clientId: process.env.TWITTER_CLIENT_ID as string,
-            clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-            version:'2.0'
-        }),
-    ],
-    // Add any additional NextAuth configuration here
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      version: "2.0",
+      authorization: {
+        url: "https://twitter.com/i/oauth2/authorize",
+        params: {
+          scope: "tweet.read tweet.write users.read offline.access",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.accessTokenExpiration = token.accessTokenExpiration =
+          Date.now() + (account.expires_at ? account.expires_at * 1000 : 0);
+      }
+
+      const accessTokenExpiration: number =
+        (token.accessTokenExpiration as number) || 0;
+
+      if (Date.now() > accessTokenExpiration) {
+        return refreshAccessToken(token);
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      session.refreshToken = token.refreshToken as string;
+      return session;
+    },
+  },
 });
+
+//https://developer.x.com/en/docs/authentication/oauth-2-0/user-access-token
+async function refreshAccessToken(token: any) {
+  try {
+    const url = "https://api.x.com/2/oauth2/token";
+    const body = new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken,
+    }).toString();
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.TWITTER_CLIENT_ID}:${process.env.TWITTER_CLIENT_SECRET}`
+        ).toString("base64")}`,
+      },
+      body,
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error("Error refreshing Twitter access token:", error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export { handler as GET, handler as POST };
