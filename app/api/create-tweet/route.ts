cb60291ecd503f734e5f9574f7b5ebb317aeac95
= <=>
2024-09-08 23:38:14 +0500

    Define route handler for tweet creation

---
+++ app/api/create-tweet/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { SendTweetV2Params, TwitterApi } from "twitter-api-v2";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const { accessToken, tweetContent, mediaId } = body as {
+    accessToken: string;
+    tweetContent: string;
+    mediaId: string;
+  };
+
+  if (!accessToken || !tweetContent) {
+    return NextResponse.json(
+      { error: "Missing accessToken or tweetContent" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const twitterClient = new TwitterApi(accessToken);
+
+    const payload: Partial<SendTweetV2Params> | undefined = mediaId
+      ? {
+          media: {
+            media_ids: [mediaId] as [string],
+          },
+        }
+      : undefined;
+
+    await twitterClient.v2.tweet(tweetContent, payload);
+
+    return NextResponse.json("Tweet has been created successfully", {
+      status: 200,
+    });
+  } catch (error: any) {
+    console.error("Error creating tweet:", error);
+    return NextResponse.json({ error: error?.message }, { status: 500 });
+  }
+}
